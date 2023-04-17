1479960a4d6e26a79177fa42e52ae8d9a2813b65
Nick-Guzy <nick.guzy76@gmail.com>
2023-04-17 13:54:11 -0700

    Weekend homwork learning react, using create react app

---
+++ src/Components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ticketsImage from "./../img/tickets.png";
+
+function Header(){
+  return (
+    <React.Fragment>
+    <h1>Help Queue</h1>
+    <img src={ticketsImage} alt="Tickets"/>
+    </React.Fragment>
+  );
+}
+export default Header;
