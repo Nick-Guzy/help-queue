729d727a6bdf34b25ccf25c307837b739183ea50
Nick-Guzy <nick.guzy76@gmail.com>
2023-05-08 16:51:10 -0700

    Add toggle theme component

--- src/Components/App.js
+++ src/Components/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import Header from "./Header";
 import SearchBar from "./SearchBar";
 import TicketControl from "./TicketControl";
+import ToggleTheme from "./ToggleTheme";
 
 function App() {
   return (
     <React.Fragment>
       <SearchBar />
       <Header />
+      <ToggleTheme />
       <TicketControl />
     </React.Fragment>
   );
